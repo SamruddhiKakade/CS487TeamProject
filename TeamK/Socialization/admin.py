82c03ca05b16e3100150ed732123825c8a5aa598
Zaid Iqbal <emailzaid@gmail.com>
2022-06-25 00:49:16 -0500

    6/25 stuff

--- TeamK/Socialization/admin.py
+++ TeamK/Socialization/admin.py
@@ -1,3 +1,16 @@
+# from django.contrib import admin
+# from models import Information,ChatMessage
+#
+# # Register your models here.
+# admin.site.register(Information)
+# admin.site.register(ChatMessage)
+
+
+
 from django.contrib import admin
+from .models import Information,ChatMessage
 
 # Register your models here.
+
+admin.site.register(Information)
+admin.site.register(ChatMessage)
--- TeamK/Socialization/models.py
+++ TeamK/Socialization/models.py
@@ -1,19 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class User(models.Model):
-    Username = models.CharField(max_length=10, unique=True)
-    Password = models.CharField(max_length=50)
+class Information(models.Model):
+    # Username = models.CharField(max_length=10, unique=True)
+    # Password = models.CharField(max_length=50)
+    # Name = models.CharField(max_length=20)
+    # Year = models.IntegerField()
+    # Major = models.CharField(max_length=100)
+
+    user = models.ForeignKey(User, related_name='+', on_delete=models.CASCADE, blank=True, null=True)
     Name = models.CharField(max_length=20)
-    Year = models.IntegerField(MinValueValidator = 1900)
-    Major = models.CharField(max_length=10, choices=open('Majors.txt').readlines())
+    Year = models.IntegerField()
+    Major = models.CharField(max_length=100)
+
+
+    # Name = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+    # Year = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+    # Major = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+
 
 class ChatMessage(models.Model):
     ChatRoomName = models.CharField(max_length=100)
-    receiver = models.CharField(max_length=10)
-    sender = models.CharField(max_length=10, unique=True)
+    receiver = models.CharField(max_length=10000)   #String containing list of users allowed to look at message
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
     message = models.CharField(max_length=1000)
     date = models.DateField()
 
 
+# class ChatRoom(models.Model):
+#     User1 =
+#     User2 =
